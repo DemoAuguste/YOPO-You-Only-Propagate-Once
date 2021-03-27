099c3d30fce226288a6f41f3ad8394d0a3cff22d
DemoAuguste <393493772@qq.com>
2021-03-27 11:41:27 +0800

    Update dataset.py

--- experiments/CIFAR10/pre-res18.pinecone/dataset.py
+++ experiments/CIFAR10/pre-res18.pinecone/dataset.py
@@ -2,7 +2,7 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
-def create_train_dataset(batch_size = 128, root = '../data'):
+def create_train_dataset(batch_size = 128, root = '../data', shuffle=True):
 
     transform_train = transforms.Compose([
      transforms.RandomCrop(32, padding=4),
@@ -13,17 +13,17 @@ def create_train_dataset(batch_size = 128, root = '../data'):
 
     trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=True, transform=transform_train)
     #trainset = torchvision.datasets.MNIST(root=root, train=True, download=True, transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=shuffle, num_workers=2)
 
     return trainloader
-def create_test_dataset(batch_size = 128, root = '../data'):
+def create_test_dataset(batch_size = 128, root = '../data', shuffle=False):
     transform_test = transforms.Compose([
      transforms.ToTensor(),
      #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     testset = torchvision.datasets.CIFAR10(root=root, train=False, download=True, transform=transform_test)
     #testset = torchvision.datasets.MNIST(root=root, train=False, download=True, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=shuffle, num_workers=2)
     return testloader
 
 
