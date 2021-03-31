1787d73c310a09e77656451bc26907df1f7c1e19
DemoAuguste <393493772@qq.com>
2021-03-31 10:38:42 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ cd $SHELL_FOLDER/experiments/CIFAR10/pre-res18.yopo-5-3/
 
 for i in {1..5}
 do
-    python3 train.py
+    # python3 train.py
     # save the result
     mkdir -p $SHELL_FOLDER/results/cifar10/res18/$i/
     echo $SHELL_FOLDER/results/cifar10/res18/$i/last.checkpoint
