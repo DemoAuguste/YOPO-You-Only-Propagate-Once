18e2587c76c774487b36acad060dcaf0d25aabe2
DemoAuguste <393493772@qq.com>
2021-03-31 10:42:39 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,13 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 echo $SHELL_FOLDER/experiments/CIFAR10/pre-res18.yopo-5-3/
 cd $SHELL_FOLDER/experiments/CIFAR10/pre-res18.yopo-5-3/
 
-for i in {1..5}
+for i in {1..10}  
+do  
+echo $(expr $i \* 3 + 1);  
+done  
+
+
+for i in {1..10}  
 do
     # python3 train.py
     # save the result
